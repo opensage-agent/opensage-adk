d7a6cf10199b1ebddcbbf0aa002e92000e893228
zhun wang <zhun.wang@berkeley.edu>
2025-09-01 19:32:04 +0000

    more tests for mock client

--- tests/utils/test_mock_client.py
+++ tests/utils/test_mock_client.py
@@ -2,6 +2,7 @@ from io import StringIO
 
 import pytest
 from google.adk.agents import LlmAgent
+from google.genai.types import Part
 
 from aigise.utils.mock_model_cli import MockModelCLI
 from aigise.utils.third_party import testing_utils
@@ -25,7 +26,24 @@ async def test_mock_model_cli(monkeypatch):
         "bye bye",
     ]
     monkeypatch.setattr("sys.stdin", StringIO("\n".join(user_inputs) + "\n"))
-    agent = LlmAgent(name="cli_agent", model=MockModelCLI(), tools=[add, multiply])
+    agent = LlmAgent(name="mock_agent", model=MockModelCLI(), tools=[add, multiply])
+    runner = testing_utils.InMemoryRunner(root_agent=agent)
+    events = await runner.run_async("What is 1 + 2 and 3 * 4?")
+    assert len(events) == 3
+    assert events[1].content.parts[0].function_response.response["result"] == 3
+
+
+@pytest.mark.asyncio
+async def test_model_model():
+    responses = [
+        Part.from_function_call(name="add", args={"x": 1, "y": 2}),
+        "bye bye",
+    ]
+    agent = LlmAgent(
+        name="mock_agent",
+        model=testing_utils.MockModel.create(responses),
+        tools=[add, multiply],
+    )
     runner = testing_utils.InMemoryRunner(root_agent=agent)
     events = await runner.run_async("What is 1 + 2 and 3 * 4?")
     assert len(events) == 3

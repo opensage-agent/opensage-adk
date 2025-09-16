2e9d339d5cb94ad5059b539267791315955d320f
zhun wang <zhun.wang@berkeley.edu>
2025-09-16 22:25:34 +0000

    update neo4j

---
+++ src/aigise/services/neo4j/client.py
@@ -0,0 +1,21 @@
+from contextlib import contextmanager
+
+from neo4j import GraphDatabase
+
+
+class Neo4JClient:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def run_query(self, query, parameters=None, database=None, **kwargs):
+        with self.driver.session(database=database) as session:
+            result = session.run(query, parameters, **kwargs)
+            return result.data()
+
+    @contextmanager
+    def session(self, database=None):
+        with self.driver.session(database=database) as session:
+            yield session
